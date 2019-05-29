71812d36d46882cc07f93235b83f8b756b93273e
Leslie Shea <leslieyshea@gmail.com>
2019-05-29 11:49:54 -0700

    wow such cool styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import CreateMeme from '../containers/memes/CreateMeme';
 import AllMemes from '../containers/memes/AllMemes';
+import styles from './styles.css';
 
 export default function Home() {
   return (
-    <>
+    <section className={styles.Home}>
       <CreateMeme />
       <AllMemes />
-    </>
+    </section>
   );
 }
--- src/components/MemePage.js
+++ src/components/MemePage.js
@@ -3,14 +3,15 @@ import DisplayMeme from '../containers/memes/DisplayMeme';
 import CommentsByMemeId from '../containers/comments/CommentsByMemeId';
 import CreateComment from '../containers/comments/CreateComment';
 import { Link } from 'react-router-dom';
+import styles from './styles.css';
 
 export default function MemePage() {
   return (
-    <>
+    <section className={styles.MemePage}>
       <Link to="/"><p>Home</p></Link>
       <DisplayMeme />
       <CreateComment />
       <CommentsByMemeId />
-    </>
+    </section>
   );
 }
