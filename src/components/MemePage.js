51a3dfd2174cd5675f66e0aed943dc06e90a7644
Leslie Shea <leslieyshea@gmail.com>
2019-05-29 11:26:54 -0700

    link back to home, we ROCK!

--- src/components/MemePage.js
+++ src/components/MemePage.js
@@ -2,10 +2,12 @@ import React from 'react';
 import DisplayMeme from '../containers/memes/DisplayMeme';
 import CommentsByMemeId from '../containers/comments/CommentsByMemeId';
 import CreateComment from '../containers/comments/CreateComment';
+import { Link } from 'react-router-dom';
 
 export default function MemePage() {
   return (
     <>
+      <Link to="/"><p>Home</p></Link>
       <DisplayMeme />
       <CreateComment />
       <CommentsByMemeId />
