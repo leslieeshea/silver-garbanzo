787c7aab7210e1d3f17de4964abf1f5142792d31
Leslie Shea <leslieyshea@gmail.com>
2019-05-28 17:42:12 -0700

    Comment pres components

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Comment({ comment }) {
+function Comment({ comment, id }) {
   return (
     <p>{comment}</p>
   );
 }
 
 Comment.propTypes = {
-  comment: PropTypes.string.isRequired
+  comment: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired
 };
 
 export default Comment;
